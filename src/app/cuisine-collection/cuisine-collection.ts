35c14e2a7beae5adbfee693f7b27f7c4456895b0
ElStephano <165284686+ElStephano@users.noreply.github.com>
2026-01-11 10:05:20 +0100

    cookbook-selection buttons

--- src/app/cuisine-collection/cuisine-collection.ts
+++ src/app/cuisine-collection/cuisine-collection.ts
@@ -2,17 +2,20 @@ import { Component, inject } from '@angular/core';
 import { FirebaseDbService, Recipe } from '../shared/service/firebase-db';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../shared/service/data.service';
-
+import { ButtonComponent } from '../shared/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cuisine-collection',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './cuisine-collection.html',
   styleUrl: './cuisine-collection.scss',
 })
+
 export class CuisineCollection {
   public firebaseDB = inject(FirebaseDbService)
   public dataService = inject(DataService)
+  private router = inject(Router)
 
   recipes: Recipe[] = [];
 
@@ -20,4 +23,7 @@ export class CuisineCollection {
     this.recipes = this.firebaseDB.currentCuisineRecipes;
   }
 
+  navigate(target: string) {
+    this.router.navigate([target])
+  }
 }
